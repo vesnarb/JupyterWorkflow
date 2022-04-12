459f2c9af52487b30b95f481a69ff41b4a3ce20d
Vesna Resende Barros <vesna@Vesnas-MacBook-Pro.local>
2022-04-12 17:45:00 +0300

    adding new functions

---
+++ jupyterworkflow/load_data.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+from urllib.request import urlretrieve
+
+URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremond_data(filename = 'Fremond.csv', url = URL, force_download = False):
+    """ Download and cache the fremond data
+
+    Parameters
+    ------------
+    filename: string (optional)
+        location to save the data
+    url: string (optional)
+        web location of the data
+    force_download: bool (optional)
+        if True, force download the data
+
+    Returns
+    ------------
+    data: pandas.DataFrame
+        The fremond bridge data
+    """
+
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, 'Fremond.csv')
+    data = pd.read_csv('Fremond.csv', index_col = 'Date', parse_dates = True)
+    return data
